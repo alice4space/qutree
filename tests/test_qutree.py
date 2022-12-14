a81e06c6f2b2573be0ea5fbdb8c4f887a539e43c
12rambau <pierrick.rambaud49@gmail.com>
2022-12-14 16:53:44 +0100

    test: prepare methods

--- tests/test_qutree.py
+++ tests/test_qutree.py
@@ -3,7 +3,7 @@ import numpy as np
 import qutree
 
 
-def test_nphi_psi():
+def test_nphi_psi() -> None:
     r = np.random.random(10)
     phi = np.random.random(10) * 2 * np.pi
 
@@ -11,3 +11,39 @@ def test_nphi_psi():
 
     assert np.allclose(r, r_test)
     assert np.allclose(phi, phi_test)
+
+
+def test_nthe_n0n1() -> None:
+    pass
+
+
+def test_phimp_phi01() -> None:
+    pass
+
+
+def test_thephi_to_xyz() -> None:
+    pass
+
+
+def fun_recursive() -> None:
+    pass
+
+
+def test_bloch_points() -> None:
+    pass
+
+
+def test_BBT() -> None:
+    pass
+
+
+def test_add_data() -> None:
+    pass
+
+
+def test_fun_recursive() -> None:
+    pass
+
+
+def test_plot_tree() -> None:
+    pass
