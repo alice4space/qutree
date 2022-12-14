a00b96415ffc8b243e56ed59bde63b87d19dc66f
12rambau <pierrick.rambaud49@gmail.com>
2022-12-14 16:49:14 +0100

    refactor: apply pre-commits

--- tests/test_qutree.py
+++ tests/test_qutree.py
@@ -1,12 +1,13 @@
 import numpy as np
-from qutree import *
+
+import qutree
 
 
 def test_nphi_psi():
     r = np.random.random(10)
     phi = np.random.random(10) * 2 * np.pi
 
-    r_test, phi_test = nphi_psi(r * np.exp(1j * phi))
+    r_test, phi_test = qutree.nphi_psi(r * np.exp(1j * phi))
 
     assert np.allclose(r, r_test)
     assert np.allclose(phi, phi_test)
